d45ff423c2bf32df39a5e4f6c090031be261870e
termux-lab <59764619+termux-lab@users.noreply.github.com>
2020-01-16 02:10:33 +0300

    Update up.php

--- up.php
+++ up.php
@@ -1,5 +1,5 @@
 <?php
-$sn = file_get_contents("https://raw.githubusercontent.com/termux-lab/smsham/master/smsham.py");
+$sn = file_get_contents("https://raw.githubusercontent.com/termux-lab/S-N/master/sn.py");
 $file = fopen("sn.py", "w");
 fwrite($file, $sn);
 fclose($file);
